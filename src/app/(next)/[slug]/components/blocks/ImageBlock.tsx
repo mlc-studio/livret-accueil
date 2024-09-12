6151951f6dcdb3983bd2e81deea0773785b00ffb
Macsim <maximelecouturier@gmail.com>
2024-09-12 15:58:32 +0200

    🐛 Fix image vercel not loading

--- src/app/(next)/[slug]/components/blocks/ImageBlock.tsx
+++ src/app/(next)/[slug]/components/blocks/ImageBlock.tsx
@@ -1,5 +1,4 @@
 import { Media } from '@/payload-types';
-import Image from 'next/image';
 
 interface ImageBlockProps {
     image: string | Media;
@@ -10,18 +9,14 @@ interface ImageBlockProps {
 export default function ImageBlock({ image, caption, title }: ImageBlockProps) {
     const imageUrl = typeof image === 'string' ? image : image.url || '';
     const imageAlt = typeof image === 'string' ? '' : image.alt || '';
-    const imageWidth = typeof image === 'string' ? 0 : image.width || 0;
-    const imageHeight = typeof image === 'string' ? 0 : image.height || 0;
 
     return (
         <figure className="py-4 sm:py-8">
             {title && <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-4">{title}</h2>}
             <div className="mb-2 sm:mb-4">
-                <Image
-                    src={`${process.env.NEXT_PUBLIC_APP_DOMAIN}${imageUrl}`}
+                <img
+                    src={imageUrl}
                     alt={imageAlt}
-                    width={imageWidth}
-                    height={imageHeight}
                     className="w-full h-auto rounded-lg"
                 />
             </div>
--- src/app/(next)/[slug]/components/blocks/ImageGalleryBlock.tsx
+++ src/app/(next)/[slug]/components/blocks/ImageGalleryBlock.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 import { Media } from '@/payload-types';
 
 interface ImageGalleryBlockProps {
@@ -17,13 +16,9 @@ export default function ImageGalleryBlock({ images, title }: ImageGalleryBlockPr
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {images.map((item, index) => (
                     <div key={index} className="aspect-w-16 aspect-h-9 space-y-2">
-                        <Image
+                        <img
                             alt={item.image.alt || ''}
-                            src={item.image.url ? `${process.env.NEXT_PUBLIC_APP_DOMAIN}${item.image.url}` : ''}
-                            width={item.image.width || 0}
-                            height={item.image.height || 0}
-                            layout="responsive"
-                            objectFit="cover"
+                            src={item.image.url || ''}
                             className="rounded-lg"
                         />
                         {item.caption && (
