dd681b45b9eb7ee3f172fc825c03c0e8749e1813
macsimtv <maximelecouturier@gmail.com>
2024-09-30 13:43:33 +0200

    🚀 Layout import from page layout

--- src/app/(next)/[locale]/layout.tsx
+++ src/app/(next)/[locale]/layout.tsx
@@ -1,28 +1,5 @@
+import LayoutTemplate from './[slug]/layout';
+
 import './global.css'
 
-import favicon from '@/assets/img/favicon.png';
-
-export const metadata = {
-  title: 'Livret d\'accueil Airbnlove',
-  description: 'Découvrez les hébergements Airbnlove et profitez de votre séjour'
-}
-
-export default function RootLayout({
-  children,
-  params
-}: {
-  children: React.ReactNode
-  params: { locale: string }
-}) {
-  return (
-    <html lang={params.locale}>
-      <head>
-        {/* Favicon */}
-        <link rel="icon" type="image/png" href={favicon.src} />
-      </head>
-      <body>
-        {children}
-      </body>
-    </html>
-  )
-}
+export default LayoutTemplate;
